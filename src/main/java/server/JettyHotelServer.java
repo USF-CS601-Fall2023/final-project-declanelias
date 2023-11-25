9ecc4e7922f357d6a06d22b0b1ae2c8375ba5011
declanelias <delias@macalester.edu>
2023-11-25 10:20:35 -0800

    JettyHotelServer set up

--- src/main/java/server/JettyHotelServer.java
+++ src/main/java/server/JettyHotelServer.java
@@ -1,9 +1,44 @@
 package server;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+
 public class JettyHotelServer {
 	public static final int PORT = 8080;
+	private Server jettyServer;
+	private ServletContextHandler handler;
+
+
+	public JettyHotelServer() {
+		jettyServer = new Server(PORT);
+	}
+
+
+	public void start() throws Exception {
+		jettyServer.start();
+		jettyServer.join();
+	}
+
+
+	public void addMapping(String path, String className){
+		// FILL IN CODE: call the method on the handler that adds the mapping between the path and the servlet
+		handler.addServlet(className, path);
+	}
+
 
 	public static void main(String[] args)  {
 		// FILL IN CODE, and add more classes as needed
+
+		JettyHotelServer server = new JettyHotelServer();
+
+		server.addMapping("/register", "RegisterServlet");
+
+		try {
+			server.start();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+
 
 	}
 }
---
+++ src/main/java/server/RegisterServlet.java
@@ -0,0 +1,22 @@
+package server;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class RegisterServlet extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        super.doGet(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        super.doPost(request, response);
+    }
+}
